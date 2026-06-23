05e71174b085cf90699af609af8e50307c3d215f
rosh <roshan.akthar30@gmail.com>
2026-06-23 17:09:39 +0530

    Updated API URL for production

--- src/components/workoutform.js
+++ src/components/workoutform.js
@@ -11,7 +11,7 @@ const WorkoutForm = ()=>{
         e.preventDefault()
         const workout = {title, load, reps}
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch(process.env.REACT_APP_API_URL + '/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext()
      useEffect(() => {
        const fetchWorkouts = async () => {
-             const response = await fetch('/api/workouts')
+             const response = await fetch(process.env.REACT_APP_API_URL + '/api/workouts')
              const json = await response.json()
 
              if (response.ok) {
